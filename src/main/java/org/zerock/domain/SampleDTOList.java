2b0c01b47e28370515b09ab9ebf7b7d946229e8a
박관준 <sinzls@naver.com>
2019-07-01 17:20:20 +0900

    코드로 배우는 스프링 웹프로젝트 파트2 xml 실습

---
+++ src/main/java/org/zerock/domain/SampleDTOList.java
@@ -0,0 +1,15 @@
+package org.zerock.domain;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import lombok.Data;
+
+@Data
+public class SampleDTOList {
+	private List<SampleDTO> list;
+	
+	public SampleDTOList() {
+		list = new ArrayList<>();
+	}
+}
